6b8c8ae024606a19897ab45671739f6e892e4a12
Alexandre Daltro <alexandrebvd@gmail.com>
2019-02-27 11:25:22 -0300

    Modifies the bar plots codes
    Using annotate instead of text to show number on bar plot graphs

--- Capstone Project.py
+++ Capstone Project.py
@@ -1,28 +1,29 @@
 
 # coding: utf-8
 
-# In[456]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import svm
-from sklearn.neighbors import NearestNeighbors
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.over_sampling import SMOTE
 from collections import Counter
-from sklearn.decomposition import PCA
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[3]:
+# In[2]:
 
 
 table = pd.read_csv('creditcard.csv')
@@ -30,7 +31,7 @@ table = pd.read_csv('creditcard.csv')
 
 # # Exploratory data analysis
 
-# In[192]:
+# In[3]:
 
 
 table.isnull().sum().all()
@@ -38,50 +39,49 @@ table.isnull().sum().all()
 
 # Null elements are not present in the dataset.
 
-# In[117]:
+# In[4]:
 
 
 table.head()
 
 
-# In[7]:
+# In[5]:
 
 
 table.tail()
 
 
-# In[8]:
+# In[6]:
 
 
 table.info()
 
 
-# In[9]:
+# In[7]:
 
 
 table.describe()
 
 
-# In[118]:
+# In[8]:
 
 
 plt.bar(['Non-Fraud','Fraud'], table['Class'].value_counts(), color=['b','r'])
 plt.xlabel('Class')
 plt.ylabel('Number of transactions')
-plt.text(0.25,0.5, '{}\n({:.4}%)'.format(table['Class'].value_counts()[0], 
-                                         table['Class'].value_counts()[0]/table['Class'].count()*100), 
-         horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-plt.text(0.75,0.5, '{}\n({:.2}%)'.format(table['Class'].value_counts()[1], 
-                                         table['Class'].value_counts()[1]/table['Class'].count()*100), 
-         horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-
+plt.annotate('{}\n({:.4}%)'.format(table['Class'].value_counts()[0], 
+                                         table['Class'].value_counts()[0]/table['Class'].count()*100),
+             (0.20, 0.45), xycoords='axes fraction')
+plt.annotate('{}\n({:.4}%)'.format(table['Class'].value_counts()[1], 
+                                         table['Class'].value_counts()[1]/table['Class'].count()*100),
+             (0.70, 0.45), xycoords='axes fraction')
 plt.tight_layout()
 plt.show()
 
 
 # The dataset has only two classes: fraud or non-fraud. The classes are highly umbalanced with 99.83% of observations belonging to non-fraudulent transactions and only 0.17% of observations labeled as fraudulent. This issue will be adressed later with a method for balancing classes.
 
-# In[193]:
+# In[9]:
 
 
 plt.scatter(table['Time']/(60*60), table['Class'])
@@ -94,7 +94,7 @@ plt.show()
 
 # The data was collected over the period of 2 days and, apparently, the 'Time' variable isn't a good predictor for frauds. As seen above, the pattern for both non-fradulent and fraudulent transactions seems to be random regarding the hour of the day.
 
-# In[197]:
+# In[10]:
 
 
 plt.boxplot(table['Amount'], labels = ['Boxplot'])
@@ -115,7 +115,7 @@ print('Number of outliers above the upper bound: ', amount[amount['Amount'] > up
 
 # 31904 outliers found using the interquartile range method, which represents 11.2% of the observations. Removing them from the dataset would be a bad idea due to the loss of a large amount of information for the machine learning models.
 
-# In[190]:
+# In[11]:
 
 
 table[table['Class']==1].where(table['Amount']>upper_bound).count()['Amount']
@@ -123,7 +123,7 @@ table[table['Class']==1].where(table['Amount']>upper_bound).count()['Amount']
 
 # In addition to that, only 91 out of 31904 outliers are classified as frauds.
 
-# In[233]:
+# In[12]:
 
 
 plt.scatter(table['Amount'], table['Class'])
@@ -132,7 +132,7 @@ plt.ylabel('Class')
 plt.show()
 
 
-# In[251]:
+# In[13]:
 
 
 target_0 = table.loc[table['Class'] == 0]
@@ -144,9 +144,10 @@ ax2.set_xlim(0, max(table[table['Class']==1]['Amount']))
 plt.legend()
 plt.xlabel('Amount')
 plt.ylabel('Density of probability')
+plt.show()
 
 
-# In[247]:
+# In[14]:
 
 
 table.loc[table['Class'] == 1]['Amount'].describe()
@@ -154,7 +155,7 @@ table.loc[table['Class'] == 1]['Amount'].describe()
 
 # We can see that fraudulent transactions are highly concentrated at smaller values when compared to non-fraudulent transactions.
 
-# In[172]:
+# In[15]:
 
 
 heatmap = sns.heatmap(table.corr(method='spearman'))
@@ -166,14 +167,14 @@ heatmap = sns.heatmap(table.corr(method='spearman'))
 
 # Before balancing the classes we need to split the observations into a training set and a testing set. ***This is extremely important!*** We can only balance the classes after we set some observations aside to be used as a test set! Otherwise, the models might use part of the test data during the training, which will lead to overfitting. Let's be smart and avoid that! :)
 
-# In[447]:
+# In[16]:
 
 
 y = table['Class']
 X = table.drop(columns=['Class'])
 
 
-# In[448]:
+# In[17]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
@@ -187,7 +188,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, rando
 
 # ### Random undersampling
 
-# In[449]:
+# In[18]:
 
 
 rus = RandomUnderSampler(sampling_strategy='auto', random_state=42, replacement=False)
@@ -196,22 +197,28 @@ X_rus, y_rus = rus.fit_resample(X_train, y_train)
 
 # Checking If classes are balanced:
 
-# In[450]:
+# In[26]:
 
 
 plt.bar(['Non-Fraud','Fraud'], [Counter(y_rus)[0], Counter(y_rus)[1]], color=['b','r'])
 plt.xlabel('Class')
 plt.ylabel('Number of transactions')
-plt.text(0.25,0.5, Counter(y_rus)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-plt.text(0.80,0.5, Counter(y_rus)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.annotate('{}'.format(Counter(y_rus)[0]), (0.25, 0.45), xycoords='axes fraction')
+plt.annotate('{}'.format(Counter(y_rus)[1]), (0.75, 0.45), xycoords='axes fraction')
 
 plt.tight_layout()
 plt.show()
 
 
+# In[27]:
+
+
+assert Counter(y_rus)[1] == Counter(y_train)[1] #Checking if they have the same number of fraud cases
+
+
 # ### Random oversampling
 
-# In[451]:
+# In[28]:
 
 
 ros = RandomOverSampler(sampling_strategy='auto', random_state=42)
@@ -220,17 +227,23 @@ X_ros, y_ros = ros.fit_resample(X_train, y_train)
 
 # Checking If classes are balanced:
 
-# In[452]:
+# In[29]:
 
 
 plt.bar(['Non-Fraud','Fraud'], [Counter(y_ros)[0], Counter(y_ros)[1]], color=['b','r'])
 plt.xlabel('Class')
 plt.ylabel('Number of transactions')
-plt.text(0.25,0.5, Counter(y_ros)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-plt.text(0.80,0.5, Counter(y_ros)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.annotate('{}'.format(Counter(y_ros)[0]), (0.25, 0.45), xycoords='axes fraction')
+plt.annotate('{}'.format(Counter(y_ros)[1]), (0.75, 0.45), xycoords='axes fraction')
 
 plt.tight_layout()
 plt.show()
+
+
+# In[30]:
+
+
+assert Counter(y_ros)[0] == Counter(y_train)[0] #Checking if they have the same number of non-fraud cases
 
 
 # ### Synthetic Minority Over-sampling (SMOTE)
@@ -241,29 +254,35 @@ plt.show()
 # 
 # [Image Source](https://www.kaggle.com/rafjaa/resampling-strategies-for-imbalanced-datasets)
 
-# In[453]:
+# In[31]:
 
 
 smote = SMOTE(sampling_strategy='auto', random_state=42)
 X_smote, y_smote = smote.fit_resample(X_train, y_train)
 
 
-# In[454]:
+# In[32]:
 
 
 plt.bar(['Non-Fraud','Fraud'], [Counter(y_smote)[0], Counter(y_smote)[1]], color=['b','r'])
 plt.xlabel('Class')
 plt.ylabel('Number of transactions')
-plt.text(0.25,0.5, Counter(y_smote)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
-plt.text(0.80,0.5, Counter(y_smote)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.annotate('{}'.format(Counter(y_smote)[0]), (0.25, 0.45), xycoords='axes fraction')
+plt.annotate('{}'.format(Counter(y_smote)[1]), (0.75, 0.45), xycoords='axes fraction')
 
 plt.tight_layout()
 plt.show()
 
 
+# In[33]:
+
+
+assert Counter(y_smote)[0] == Counter(y_train)[0] #Checking if they have the same number of non-fraud cases
+
+
 # #### Checking the difference between random oversampling and SMOTE
 
-# In[455]:
+# In[34]:
 
 
 def plot_2d_space(X, y, label='Classes'):
@@ -279,8 +298,10 @@ def plot_2d_space(X, y, label='Classes'):
 
 # Because the dataset has many features and our graphs will be 2D, we will reduce the size of the dataset using Principal Component Analysis (PCA):
 
-# In[457]:
+# In[35]:
 
+
+std_scale = StandardScaler().fit(X_train)
 
 pca = PCA(n_components=2)
 X_ros_pca = pca.fit_transform(X_ros)
@@ -291,3 +312,31 @@ plot_2d_space(X_smote_pca, y_smote, 'Balanced dataset (2 PCA components) using S
 
 
 # If you look closely, you can see that some of the observations tagged as 1 (fraudulent transactions) are present in different coordinates on the graphs above. This is due to the new fraudulent observations created by the SMOTE algorithm.
+
+# # Models
+
+# Before we begin let's first create a function to perform feature scaling because some models need this prior to fitting.
+
+# In[ ]:
+
+
+def feature_scaling(X_train, X_test=X_test):
+    std_scale = StandardScaler().fit(X_train)
+    X_train_std = std_scale.transform(X_train)
+    X_test_std = std_scale.transform(X_test)
+    return X_train_std, X_test_std
+
+
+# ## Classification algorithms
+
+# In[ ]:
+
+
+models = []
+
+models.append(('LR', LogisticRegression()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('SVM', SVC()))
+models.append(('GNB', GaussianNB()))
+models.append(('RF', RandomForestClassifier()))
+
