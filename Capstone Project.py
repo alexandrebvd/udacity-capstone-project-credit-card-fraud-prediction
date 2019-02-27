e2f3b4422ac47ed2c941a0904239efcc1661775f
Alexandre Daltro <alexandrebvd@gmail.com>
2019-02-26 22:44:45 -0300

    Adds balancing of classes (undersampling, oversampling and SMOTE)

--- Capstone Project.py
+++ Capstone Project.py
@@ -1,13 +1,24 @@
 
 # coding: utf-8
 
-# In[116]:
+# In[456]:
 
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.neighbors import NearestNeighbors
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+from sklearn.decomposition import PCA
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
@@ -121,7 +132,7 @@ plt.ylabel('Class')
 plt.show()
 
 
-# In[249]:
+# In[251]:
 
 
 target_0 = table.loc[table['Class'] == 0]
@@ -151,4 +162,129 @@ heatmap = sns.heatmap(table.corr(method='spearman'))
 
 # We can see that all features have very low correlation coefficients among each other.
 
-# # Balancing the dataset
+# # Balancing the classes
+
+# Before balancing the classes we need to split the observations into a training set and a testing set. ***This is extremely important!*** We can only balance the classes after we set some observations aside to be used as a test set! Otherwise, the models might use part of the test data during the training, which will lead to overfitting. Let's be smart and avoid that! :)
+
+# In[447]:
+
+
+y = table['Class']
+X = table.drop(columns=['Class'])
+
+
+# In[448]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
+
+
+# We are going to test three types of resampling methods: [random undersampling](https://imbalanced-learn.readthedocs.io/en/stable/generated/imblearn.under_sampling.RandomUnderSampler.html#imblearn.under_sampling.RandomUnderSampler), [random oversampling](https://imbalanced-learn.readthedocs.io/en/stable/generated/imblearn.over_sampling.RandomOverSampler.html#imblearn.over_sampling.RandomOverSampler) and [Synthetic Minority Over-sampling (SMOTE)](https://imbalanced-learn.readthedocs.io/en/stable/generated/imblearn.over_sampling.SMOTE.html#imblearn.over_sampling.SMOTE). The image below depicts how undersampling and oversampling works and it is very intuitive to understand. SMOTE is similar to oversampling but instead of copying the same original points randomly, the algorithm creates new points close to the original ones.
+
+# ![image.png](attachment:image.png)
+# [Image Source](https://www.kaggle.com/rafjaa/resampling-strategies-for-imbalanced-datasets)
+
+# ### Random undersampling
+
+# In[449]:
+
+
+rus = RandomUnderSampler(sampling_strategy='auto', random_state=42, replacement=False)
+X_rus, y_rus = rus.fit_resample(X_train, y_train)
+
+
+# Checking If classes are balanced:
+
+# In[450]:
+
+
+plt.bar(['Non-Fraud','Fraud'], [Counter(y_rus)[0], Counter(y_rus)[1]], color=['b','r'])
+plt.xlabel('Class')
+plt.ylabel('Number of transactions')
+plt.text(0.25,0.5, Counter(y_rus)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.text(0.80,0.5, Counter(y_rus)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+
+plt.tight_layout()
+plt.show()
+
+
+# ### Random oversampling
+
+# In[451]:
+
+
+ros = RandomOverSampler(sampling_strategy='auto', random_state=42)
+X_ros, y_ros = ros.fit_resample(X_train, y_train)
+
+
+# Checking If classes are balanced:
+
+# In[452]:
+
+
+plt.bar(['Non-Fraud','Fraud'], [Counter(y_ros)[0], Counter(y_ros)[1]], color=['b','r'])
+plt.xlabel('Class')
+plt.ylabel('Number of transactions')
+plt.text(0.25,0.5, Counter(y_ros)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.text(0.80,0.5, Counter(y_ros)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+
+plt.tight_layout()
+plt.show()
+
+
+# ### Synthetic Minority Over-sampling (SMOTE)
+
+# A quick explanation of how SMOTE works: it consists of synthesizing elements for the minority class using the existing ones. It randomly chooses a point from the minority class and computes the k-nearest neighbors (default = 5) for this point. The synthetic points are added between the chosen point and its neighbors by choosing a factor between 0 and 1 to multiply the distance. This process can be seen below.
+
+# ![image.png](attachment:image.png)
+
+# In[453]:
+
+
+smote = SMOTE(sampling_strategy='auto', random_state=42)
+X_smote, y_smote = smote.fit_resample(X_train, y_train)
+
+
+# In[454]:
+
+
+plt.bar(['Non-Fraud','Fraud'], [Counter(y_smote)[0], Counter(y_smote)[1]], color=['b','r'])
+plt.xlabel('Class')
+plt.ylabel('Number of transactions')
+plt.text(0.25,0.5, Counter(y_smote)[0], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+plt.text(0.80,0.5, Counter(y_smote)[1], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+
+plt.tight_layout()
+plt.show()
+
+
+# #### Checking the difference between random oversampling and SMOTE
+
+# In[455]:
+
+
+def plot_2d_space(X, y, label='Classes'):
+    '''Plots the data points in a 2D scatterplot.'''
+    colors = ['blue', 'red']
+    markers = ['o', 's']
+    for l, c, m in zip(np.unique(y), colors, markers):
+        plt.scatter(X[y==l, 0], X[y==l, 1], c=c, label=l, marker=m)
+    plt.title(label)
+    plt.legend(loc='best')
+    plt.show()
+
+
+# Because the dataset has many features and our graphs will be 2D, we will reduce the size of the dataset using Principal Component Analysis (PCA):
+
+# In[457]:
+
+
+pca = PCA(n_components=2)
+X_ros_pca = pca.fit_transform(X_ros)
+X_smote_pca = pca.fit_transform(X_smote)
+
+plot_2d_space(X_ros_pca, y_ros, 'Balanced dataset (2 PCA components) using random oversampling')
+plot_2d_space(X_smote_pca, y_smote, 'Balanced dataset (2 PCA components) using SMOTE')
+
+
+# If you look closely, you can see that some of the observations tagged as 1 (fraudulent transactions) are present in different coordinates on the graphs above. This is due to the new fraudulent observations created by the SMOTE algorithm.
